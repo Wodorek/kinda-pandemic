839bfef2ba1d50249baf6234626695579e4435f1
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-11-09 13:55:38 +0100

    Outbreak marker added

--- Outbreaks.gd
+++ Outbreaks.gd
@@ -1,8 +1,12 @@
 extends Control
 
+#find the number programatically later?
+
+var marker_offset:Vector2 = Vector2(5,8)
+var marker_idx:int = 0
 
 func _ready():
-	var points_range = $OutbreakTrack.curve.get_point_count()
+	var points_range = $OutbreakTrack.curve.get_point_count() 
 	
 	for point in points_range:
 		var new_label = Label.new()
@@ -12,3 +16,11 @@ func _ready():
 		
 		if point == 8:
 			new_label.text = "X"
+	
+	$Marker.position = $OutbreakTrack.curve.get_point_position(0) + marker_offset
+
+
+
+func _on_Button_pressed():
+	marker_idx += 1
+	$Marker.position = $OutbreakTrack.curve.get_point_position(marker_idx) + marker_offset
