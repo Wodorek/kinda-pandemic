9667b9c5f26862055fad8e14802ea84e78df883d
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-11-15 17:38:14 +0100

    Folder restructure

--- UI/InfectionTrack.gd
+++ UI/InfectionTrack.gd
@@ -6,9 +6,12 @@ var infection_values = [2,2,2,3,3,4,4]
 func _ready():
 
 	var points_number = $TrackCurve.curve.get_point_count()
+	get_font("gunplay").size = 20
+	$Marker.position = $TrackCurve.curve.get_point_position(0) - Vector2(14,8)
 	
 	for point in points_number -1:
 		var new_label = Label.new()
 		new_label.text = str(infection_values[point])
 		new_label.rect_position = $TrackCurve.curve.get_point_position(point)
+		
 		add_child(new_label)
