5c894bba841cf90a865256cce43859d17290eaca
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-11-18 00:09:39 +0100

    Nodes data extracted

--- Board.gd
+++ Board.gd
@@ -1,8 +1,19 @@
 extends Node2D
 
+var path = "user://data.json"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	var nodes_data = {}
+	
 	for child in get_children():
 		if child.is_class("Position2D"):
-			print(child.name,child.position)
+			nodes_data[child.name] = child.position
+
+	var file = File.new()
+	file.open(path, File.WRITE)
+	file.store_line(to_json(nodes_data))
+	file.close()
+	
+	
