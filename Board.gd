190102e6be56ec3475a6104006dc3eb1bda0e547
Wodorek <58212786+Wodorek@users.noreply.github.com>
2022-11-18 00:22:45 +0100

    Reading nodes from file

--- Board.gd
+++ Board.gd
@@ -4,5 +4,8 @@ var path = "user://data.json"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var file = File.new()
+	file.open('res://data/nodesData.json', File.READ)
 	
+	var nodes = parse_json(file.get_as_text())
 
